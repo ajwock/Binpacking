4d5f11dfa4c9dc22c1726ea2043b364ccdd5cb87
gjreynol <gjreynol@ncsu.edu>
2020-04-15 12:09:43 -0400

    Updated Bins with greater functionality

--- Binpacking Problem/src/snippet/Bin.java
+++ Binpacking Problem/src/snippet/Bin.java
@@ -1,8 +1,51 @@
 package snippet;
 
-public class Bin {
+import java.util.ArrayList;
 
-	int remainingSpace() {
-		return 0;
+public class Bin {
+	/** The capacity of the bin */
+	private int cap;
+	/** The total weight of items that are currently stored in this bin */
+	private int weight;
+	/** The total number of items that are currently packed in the bin */
+	private int packed;
+	/** An ArrayList of items in this bin */
+	ArrayList<Item> packedList;
+
+	Bin(int capacity) {
+		cap = capacity;
+		packedList = new ArrayList<Item>();
+		packed = 0;
+		weight = 0;
 	}
+
+	public int remainingSpace() {
+		return cap-weight;
+	}
+	
+	private int gotSpace(int itemWeight) {
+		int free = cap - weight;
+		if (free >= itemWeight) {
+			return free;
+		} else {
+			return -1;
+		}
+	}
+
+	public void addItem(Item g) {
+		if (gotSpace(g.getWeight()) != -1) {
+			this.weight += g.getWeight();
+			packedList.add(g);
+			packed++;
+		}
+	}
+
+	public int getPacked() {
+		return this.packed;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
 }
