2bcbe26c93437c4ef17cf4a61511417cc56eaeef
Matthias Stallmann <mfms@ncsu.edu>
2020-04-20 11:34:27 -0400

    comments added

--- Binpacking Problem/src/snippet/BinPackingModel.java
+++ Binpacking Problem/src/snippet/BinPackingModel.java
@@ -2,6 +2,9 @@ package snippet;
 
 import java.util.List;
 
+/**
+ * Keeps track of the global upper bound.
+ */
 public class BinPackingModel {
 
 	public List<Bin> solution;
@@ -39,3 +42,5 @@ public class BinPackingModel {
 		return solution;
 	}
 }
+
+//  [Last modified: 2020 04 20 at 15:10:38 GMT]
